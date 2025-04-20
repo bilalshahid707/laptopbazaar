c20bc150a221ed4d5ce77c1419bebb6880acb7e2
Muhammad Bilal <bilalshahid187@gmail.com>
2025-04-20 21:31:18 +0500

    bug fixes

--- backend/controllers/laptopController.js
+++ backend/controllers/laptopController.js
@@ -80,15 +80,6 @@ exports.getStats = catchAsync(async (req, res, next) => {
     {
       $count: "totalLaptops",
     },
-    {
-      $group:{
-        _id:'$brand',
-        count: { $sum: 1 }
-      }
-    },{
-      $sort:{count:-1}
-    },
-
   ]);
   res.status(200).json({
     status: "success",
